3697069dd6ede2aa74784d8936d44954362285dc
GitHub Action <action@github.com>
2022-09-07 09:36:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/segreeeen/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/segreeeen/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.segreeeen.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.segreeeen.testplugin.MyBundle
 
 class MyApplicationService {
 
